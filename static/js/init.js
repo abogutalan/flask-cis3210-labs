9b8053e2b85ac1ed47ac5290a399baf406d58bd4
Abdullah Ogutalan <aogutala@cis3210.socs.uoguelph.ca>
2020-11-16 23:40:03 -0500

    finished lab10

--- static/js/init.js
+++ static/js/init.js
@@ -24,6 +24,7 @@ $(document).ready(function () {
   // document.getElementById("put-button").addEventListener("click", put_method);
   // document.getElementById("delete-button").addEventListener("click", delete_method);
   document.getElementById("show-button").addEventListener("click", show_method);
+  document.getElementById("exclude-button").addEventListener("click", exclude_method);
 
   function show_method() {
     // https://codeburst.io/multiple-ways-of-implementing-flickr-public-api-in-jquery-and-javascript-dbaf0f35bbef
@@ -84,6 +85,61 @@ $(document).ready(function () {
     e.stopPropagation();
     $(this).remove();
 });
+
+function exclude_method() {
+  // https://codeburst.io/multiple-ways-of-implementing-flickr-public-api-in-jquery-and-javascript-dbaf0f35bbef
+  console.log("get bttn clicked!");
+  var flickerAPI = "https://api.flickr.com/services/feeds/photos_public.gne?format=json&tags!=" + $("#search").val();
+  $.ajax({
+    url: flickerAPI,
+    dataType: "jsonp", // jsonp
+    jsonpCallback: 'jsonFlickrFeed', // add this property
+    success: function (result, status, xhr) {
+      document.getElementById("outputDiv").innerHTML = "";
+      let quantity = $('#quantity').val();
+      let outputDiv = document.getElementById('outputDiv');
+      
+      $.each(result.items, function (i, item) {
+        console.log('item')
+        console.log(item)
+        // $("<img>").attr("src", item.media.m).appendTo("#outputDiv");
+        let a = document.createElement('a');
+        let img = document.createElement('img');
+        img.src = item.media.m;
+        
+        a.appendChild(img);                    
+        a.href = item.media.m;
+        a.download = item.media.m;
+
+        img.height = 100;
+        img.width = 100;
+        outputDiv.appendChild(a);
+
+        if (i == quantity-1) {
+          return false;
+        }
+      });
+      $('#outputDiv').each(function() {
+        if (!$(this).find('img').length) {
+            // there is an image in this div, do something...
+            console.log("img does not exist!")
+            document.getElementById("outputDiv").innerHTML = "img does not exist!";
+        }
+    });
+    },
+    error: function (xhr, status, error) {
+      console.log(xhr)
+      $("#outputDiv").html("Result: " + status + " " + error + " " + xhr.status + " " + xhr.statusText)
+    }
+  });
+  // $.ajax({
+  //   url: "/images", 
+  //   data: 'https://live.staticflickr.com/689/22847153945_b769a2b78f_o.jpg', 
+  //   type: 'get', success: function (result) {
+  //     $("#result-text").html(result);
+  //   }
+  // });
+}
 
   function get_method() {
 
